0be08ae900c902c00b1d08bde7ee08181be38689
Anselm Joseph <an23lm@gmail.com>
2021-03-17 01:07:58 +0530

    logout and otp entry

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useContext } from "react";
+import { View, Text, Button } from "react-native";
+import AuthContext from "../../context/AuthContext";
 
 export default function HomeScreen() {
+  const { authState, authActions } = useContext(AuthContext);
+
+  const logout = () => {
+    authActions.logout();
+  };
+
   return (
     <View>
       <Text>Home</Text>
+      <Button title={"LOGOUT"} onPress={logout} />
     </View>
-  )
+  );
 }
--- src/screens/RegisterPhoneScreen.tsx
+++ src/screens/RegisterPhoneScreen.tsx
@@ -1,52 +1,69 @@
-import { FirebaseRecaptchaBanner, FirebaseRecaptchaVerifierModal } from 'expo-firebase-recaptcha'
-import firebase from 'firebase'
-import React, { useRef, useState } from 'react'
-import { View, Text, TextInput, Button } from 'react-native'
-import { IFirebaseOptions } from 'expo-firebase-core';
+import {
+  FirebaseRecaptchaBanner,
+  FirebaseRecaptchaVerifierModal,
+} from "expo-firebase-recaptcha";
+import firebase from "firebase";
+import React, { useContext, useRef, useState } from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import { IFirebaseOptions } from "expo-firebase-core";
+import AuthContext from "../../context/AuthContext";
+import OTPInputView from "@twotalltotems/react-native-otp-input";
 
 export default function RegisterPhoneScreen() {
-
-  const firebaseRecaptchaVerifier = useRef(null)
-  const firebaseConfig = firebase.apps.length ? firebase.app().options as IFirebaseOptions : undefined
+  const firebaseRecaptchaVerifier = useRef(null);
+  const firebaseConfig = firebase.apps.length
+    ? (firebase.app().options as IFirebaseOptions)
+    : undefined;
   const attemptInvisibleVerification = true;
 
-  const [phoneNumber, setPhoneNumber] = useState<string>("+")
-  const [otp, setOtp] = useState<string>("")
-  const [message, setMessage] = useState(!firebaseConfig ? "Invalid firebase config" : "")
-  const [verificationId, setVerificationId] = useState<null | string>(null)
+  const [phoneNumber, setPhoneNumber] = useState<string>("+");
+  const [otp, setOtp] = useState<string>("");
+  const [message, setMessage] = useState(
+    !firebaseConfig ? "Invalid firebase config" : ""
+  );
+  const [verificationId, setVerificationId] = useState<null | string>(null);
+
+  const { authState, authActions } = useContext(AuthContext);
 
   const sendOtp = async () => {
     if (phoneNumber && phoneNumber.length == 13) {
       try {
-        const phoneAuthProvider = new firebase.auth.PhoneAuthProvider()
+        const phoneAuthProvider = new firebase.auth.PhoneAuthProvider();
         const verificationId = await phoneAuthProvider.verifyPhoneNumber(
           phoneNumber,
           firebaseRecaptchaVerifier.current!
-        )
-        setVerificationId(verificationId)
-        setMessage("OTP has been sent")
+        );
+        setVerificationId(verificationId);
+        setMessage("OTP has been sent");
       } catch (err) {
-        setMessage("Error: " + err.message)
+        setMessage("Error: " + err.message);
       }
     } else {
-      setMessage("Invalid Phone Number")
+      setMessage("Invalid Phone Number");
     }
-  }
+  };
 
   const verifyOtp = async () => {
     if (verificationId) {
       try {
-        const credential = firebase.auth.PhoneAuthProvider.credential(verificationId!, otp)
-        await firebase.auth().signInWithCredential(credential)
-        setMessage("Login Success")
+        const credential = firebase.auth.PhoneAuthProvider.credential(
+          verificationId!,
+          otp
+        );
+        let userCredential = await firebase
+          .auth()
+          .signInWithCredential(credential);
+        setMessage("Login Success");
+
+        authActions.login(userCredential.user!.uid);
       } catch (err) {
-        console.log("login failed", err.message)
-        setMessage("Error: " + err.message)
+        console.log("login failed", err.message);
+        setMessage("Error: " + err.message);
       }
     } else {
-      setMessage("Send OTP first")
+      setMessage("Send OTP first");
     }
-  }
+  };
 
   return (
     <View>
@@ -68,13 +85,18 @@ export default function RegisterPhoneScreen() {
       <Button title="SEND OTP" onPress={sendOtp} />
 
       <Text>Enter OTP</Text>
-      <TextInput
-        onChangeText={setOtp}
-        style={{ borderWidth: 1, borderColor: "#333", padding: 5 }}
+      <OTPInputView
+        style={{ width: "80%", height: 200 }}
+        pinCount={6}
+        code={otp}
+        onCodeChanged={setOtp}
+        autoFocusOnLoad
+        onCodeFilled={verifyOtp}
       />
+
       <Button title="LOGIN" onPress={verifyOtp} />
 
       {attemptInvisibleVerification && <FirebaseRecaptchaBanner />}
     </View>
-  )
+  );
 }
